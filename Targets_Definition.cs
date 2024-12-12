7b8f4a145764930e8c04f5687e398d5b53e815ab
Lacazek <152266244+Lacazek@users.noreply.github.com>
2024-12-12 14:25:43 +0100

    Add files via upload

--- Targets_Definition.cs
+++ Targets_Definition.cs
@@ -1,4 +1,30 @@
-﻿using Structure_optimisation;
+﻿/******************************************************************************
+ * Nom du fichier : Target_Definition.cs
+ * Auteur         : LACAZE Killian
+ * Date de création : [02/10/2024]
+ * Description    : [Brève description du contenu ou de l'objectif du code]
+ *
+ * Droits d'auteur © [2024], [LACAZE.K].
+ * Tous droits réservés.
+ * 
+ * Ce code a été développé exclusivement par LACAZE Killian. Toute utilisation de ce code 
+ * est soumise aux conditions suivantes :
+ * 
+ * 1. L'utilisation de ce code est autorisée uniquement à titre personnel ou professionnel, 
+ *    mais sans modification de son contenu.
+ * 2. Toute redistribution, copie, ou publication de ce code sans l'accord explicite 
+ *    de l'auteur est strictement interdite.
+ * 3. L'auteur assume la responsabilité de l'utilisation de ce code dans ses propres projets.
+ * 
+ * CE CODE EST FOURNI "EN L'ÉTAT", SANS AUCUNE GARANTIE, EXPRESSE OU IMPLICITE. 
+ * L'AUTEUR DÉCLINE TOUTE RESPONSABILITÉ POUR TOUT DOMMAGE OU PERTE RÉSULTANT 
+ * DE L'UTILISATION DE CE CODE.
+ *
+ * Toute utilisation non autorisée ou attribution incorrecte de ce code est interdite.
+ ******************************************************************************/
+
+
+using Structure_optimisation;
 using System.Collections.Generic;
 
 
